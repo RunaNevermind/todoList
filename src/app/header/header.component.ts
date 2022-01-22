54d733a902c732136c1c3b1f742e9bf1f3b51ba5
RunaNevermind <79098228+RunaNevermind@users.noreply.github.com>
2022-01-22 21:56:16 +0300

    final with css

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(public user:UserService) { }
+  public userEmail:string=this.user.admin.email;
+  public userPassword:string=this.user.admin.password;
+  public removeUser:boolean=false;
 
-  ngOnInit(): void {
-  }
 
 }
